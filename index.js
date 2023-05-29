28074b4a5b5333d3fcdb96dd0605567208c16cef
NS-Sheam <123sheamfeni@gmail.com>
2023-05-30 00:33:23 +0600

    admin dashboard running

--- index.js
+++ index.js
@@ -34,13 +34,38 @@ async function run() {
 
 
         // users related api 
+
+        app.get("/users", async (req, res) =>{
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+
+
         app.post("/users", async (req, res) => {
             const user = req.body;
             // console.log(user);
+            const query = {email: user.email};
+            const existingUser = await usersCollection.findOne(query);
+            // console.log("existing user", existingUser);
+            if (existingUser) {
+                return res.send({message: "user already exist"});
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result);
             // console.log(result);
-        })
+        });
+
+        app.patch("/users/admin/:id", async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                  role: "admin"
+                },
+              };
+
+              const result = await usersCollection.updateOne(filter, updateDoc);
+              res.send(result);
 
         // menu related api 
         app.get("/menu", async (req, res) => {
